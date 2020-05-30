397f53413350bab6f7b344a055c8e8cd8a583ba1
XanderJL <alexanderjameslow@gmail.com>
2020-05-30 16:16:04 -0400

    refactored: adjusted section component logic and added styling

--- site/src/components/Section.js
+++ site/src/components/Section.js
@@ -3,16 +3,56 @@ import BackgroundImage from "gatsby-background-image"
 import PortableText from "@sanity/block-content-to-react"
 import ReactPlayer from "react-player"
 
-const Section = ({ url, title, body }) => {
-  return (
-    <section className="section">
-      <div className="container">
-        <div className="media">
-          <ReactPlayer url={url} controls />
-        </div>
+const Section = ({ bgImage, url, title, body }) => {
+  const ParagraphRenderer = props => {
+    const { style = "normal" } = props.node
+    if (style === "normal") {
+      return <p className="is-family-monospace">{props.children}</p>
+    }
+    // Fall back to default handling
+    return PortableText.defaultSerializers.types.block(props)
+  }
+  return bgImage ? (
+    <BackgroundImage
+      className="section"
+      fluid={bgImage}
+      style={{
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <div className="container has-text-white">
+        {url ? (
+          <div className="media">
+            <ReactPlayer url={url} controls />
+          </div>
+        ) : null}
         <div className="content">
-          <h2 className="title is-size-4">{title}</h2>
-          <PortableText blocks={body} />
+          <h2 className="title is-size-3 is-uppercase has-text-white">
+            {title}
+          </h2>
+          <PortableText
+            blocks={body}
+            serializers={{ types: { block: ParagraphRenderer } }}
+          />
+        </div>
+      </div>
+    </BackgroundImage>
+  ) : (
+    <section class="section">
+      <div class="container has-text-white">
+        {url ? (
+          <div className="media">
+            <ReactPlayer url={url} controls />
+          </div>
+        ) : null}
+        <div className="content">
+          <h2 className="title is-size-3 is-uppercase has-text-white">
+            {title}
+          </h2>
+          <PortableText
+            blocks={body}
+            serializers={{ types: { block: ParagraphRenderer } }}
+          />
         </div>
       </div>
     </section>
